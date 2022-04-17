7490a5c507dd824f9957078b2697680e36d84b7b
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-17 23:24:36 +0200

    Improve code

--- src/settings.h
+++ src/settings.h
@@ -2,20 +2,20 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 // M5Stack Board
-#define BOARD CORE2                             // Your M5Stack Board
+#define BOARD BASIC                             // Your M5Stack Board
 
 // IC Transceiver Model
-#define IC_MODEL 7300                          // 705, 7300 (and may be 9700)
+#define IC_MODEL 705                            // 705, 7300 (and may be 9700)
 
 // IC Connect
-#define IC_CONNECT  USB                        // BT (Bluetooth) or USB (Universal Serial Bus)
+#define IC_CONNECT  BT                          // BT (Bluetooth) or USB (Universal Serial Bus)
 
 // CI_V Address 
 #define CI_V_ADDRESS 0xA4                       // CI-V default address 0xA4
 
 // Wifi Config
-#define WIFI_SSID       "F1ZPX"        // Your WiFi SSID
-#define WIFI_PASSWORD   "petitchaton"    // Your WiFi Password
+#define WIFI_SSID       "YOUR WIFI SSID"        // Your WiFi SSID
+#define WIFI_PASSWORD   "YOUR WIFI PASSWORD"    // Your WiFi Password
 
 // Proxy Config
 #define SERIAL_DEVICE "/dev/ttyUSB0"            // Proxy Serial Device
