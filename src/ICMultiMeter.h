365e60af021d2a649238cb4cd9b5f7801a42c522
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-04-24 06:12:21 +0200

    Improve espressif32

--- src/ICMultiMeter.h
+++ src/ICMultiMeter.h
@@ -1,7 +1,7 @@
 // Copyright (c) F4HWN Armel. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#define VERSION "0.0.4"
+#define VERSION "0.0.5"
 #define AUTHOR "F4HWN"
 #define NAME "ICMultiMeter"
 
