e6e58a243c93a92730457e435c0095756dee5f29
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-05-01 14:18:49 +0200

    Refactoring and update donors

--- src/ICMultiMeter.h
+++ src/ICMultiMeter.h
@@ -118,7 +118,7 @@ uint8_t binIndex = 0;
 
 // Menu
 const char *settings[] = {"Voice TX", "Brightness", "Transverter Mode", "IP Address", "Beep", "Shutdown", "Exit"};
+const char *choiceVoice[] = {"OFF", "T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8"};
 const char *choiceBrightness[] = {"BRIGHTNESS"};
 const char *choiceTransverter[] = {"OFF", "ON"};
 const char *choiceBeep[] = {"OFF", "ON"};
-const char *choiceVoice[] = {"OFF", "T1", "T2", "T3", "T4", "T5", "T6", "T7", "T8"};
